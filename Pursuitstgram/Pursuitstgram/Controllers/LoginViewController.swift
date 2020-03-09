5fcde505a2fd286664d7ac0c26c59a985176fdaa
CameronRivera <cameronrivera@pursuit.org>
2020-03-09 16:17:52 -0400

    Initial Firebase set up complete.

---
+++ Pursuitstgram/Pursuitstgram/Controllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Pursuitstgram
+//
+//  Created by Cameron Rivera on 3/9/20.
+//  Copyright © 2020 Cameron Rivera. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Pursuitstgram/Pursuitstgram/Views/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  Pursuitstgram
+//
+//  Created by Cameron Rivera on 3/9/20.
+//  Copyright © 2020 Cameron Rivera. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
